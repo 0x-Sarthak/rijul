2316aef77a7a8967e5594ae77acb6cfc7346ded6
0x-Sarthak <sarthakahuja447@gmail.com>
2022-12-19 20:18:39 +0530

    Minor Updates for Rijul

--- src/screens/About/index.js
+++ src/screens/About/index.js
@@ -4,52 +4,54 @@ import Header from "../../components/Header";
 import "./about.scss";
 
 const About = () => {
-  return (
-    <>
-      <Header />
-      <div className="about-container">
-        <div>
-          <img src={Girl} alt="Hi.png" />
-        </div>
-        <div className="text-area">
-          <h1>Daughter Dreamer Designer</h1>
-          <p>
-            My name is Rijul Jain, currently working as an intern in Fareportal.
-            I started my jouney as a computer engineer but found solace in
-            design. Throughout my studies, I worked on a variety of briefs,
-            including web design, user interface design, branding, and graphic
-            design. My group projects aided in the development of my teamwork
-            and communication abilities.
-          </p>
+	return (
+		<>
+			<Header />
+			<div className="about-container">
+				<div>
+					<img src={Girl} alt="Hi.png" />
+				</div>
+				<div className="text-area">
+					<h1>Daughter Dreamer Designer</h1>
+					<p>
+						My name is Rijul Jain, currently working as an intern in Fareportal.
+						I started my jouney as a computer engineer but found solace in
+						design. Throughout my studies, I worked on a variety of briefs,
+						including web design, user interface design, branding, and graphic
+						design. My group projects aided in the development of my teamwork
+						and communication abilities.
+					</p>
 
-          <h4> My Beliefs</h4>
-          <div>
-            <h5> Everything revolves around passion. </h5>
-            <p>
-              I believe that working with passion is extremely important. It not
-              only keeps us motivated and optimistic, but it also serves as a
-              learning booster.
-            </p>
-          </div>
-          <div>
-            <h5>Be kind.</h5>
-            <p>
-              Kindness has the power to change the world for the better. Even
-              the most insignificant acts of kindness leave their imprints on
-              the hearts of others.
-            </p>
-          </div>
-          <div>
-            <h5>Simplicity is the ultimate sophistication.</h5>
-            <p>
-            Simplicity facilitates the smooth flow of things, whether at work or in life. It produces open-mindedness and clarifies our priorities. Simply put, simplicity simplifies life.
-            </p>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+					<h4> My Beliefs</h4>
+					<div>
+						<h5> Everything revolves around passion. </h5>
+						<p>
+							I believe that working with passion is extremely important. It not
+							only keeps us motivated and optimistic, but it also serves as a
+							learning booster.
+						</p>
+					</div>
+					<div>
+						<h5>Be kind.</h5>
+						<p>
+							Kindness has the power to change the world for the better. Even
+							the most insignificant acts of kindness leave their imprints on
+							the hearts of others.
+						</p>
+					</div>
+					<div>
+						<h5>Simplicity is the ultimate sophistication.</h5>
+						<p>
+							Simplicity facilitates the smooth flow of things, whether at work
+							or in life. It produces open-mindedness and clarifies our
+							priorities. Simply put, simplicity simplifies life.
+						</p>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default About;
--- src/screens/Resume/index.js
+++ src/screens/Resume/index.js
@@ -1,106 +1,125 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import "./resume.scss";
-import {useInView} from "react-intersection-observer";
+import { useInView } from "react-intersection-observer";
 const Resume = () => {
-	const { ref: volunteer, inView: volunteerIsVisible } = useInView({threshold: 0.4});
-	const { ref: education, inView: educationIsVisible } = useInView({threshold: 0.8});
-	const { ref: experience, inView: experienceIsVisible } = useInView({threshold: 0.3});
+	const { ref: volunteer, inView: volunteerIsVisible } = useInView({
+		threshold: 0.7,
+	});
+	const { ref: education, inView: educationIsVisible } = useInView({
+		threshold: 1,
+	});
+	const { ref: experience, inView: experienceIsVisible } = useInView({
+		threshold: 0.2,
+	});
 
-	  const [active, setActive] = useState('');
+	const [active, setActive] = useState("");
 
-	  useEffect(() => {
-		if(educationIsVisible) setActive('education')
-		if(volunteerIsVisible) setActive('volunteer')
-		if(experienceIsVisible) setActive('experience')
-		
-	  }, [])
+	useEffect(() => {
+		if (educationIsVisible) setActive("education");
+		if (volunteerIsVisible) setActive("volunteer");
+		if (experienceIsVisible) setActive("experience");
+	}, []);
 	return (
 		<div style={{ textAlign: "center" }}>
 			<Header />
 			<div className="resume-container">
-			<div className="reactive-container">
-				<h3 className={experienceIsVisible ? "reactive" : ''}>Work Experience</h3>
-				<h3 className={volunteerIsVisible ? "reactive" : ''}>Volunteering and Leadership</h3>
-				<h3 className={educationIsVisible ? "reactive" : ''}>Education</h3>
-				<h3>Resume</h3>
-			</div>
-			<div className="text-area">
-			<div ref={experience}>
-			<h2> UI/UX Designer Intern</h2>
-			<h4>Foreportal India Private Limited Gurgaon</h4>
-			<h5>20-06-2022 to 20-12-2022</h5>
-			
-			<ul>
-			<li>Carried out competitive analysis, qualitative research, brainstorming, and ideation for Black Friday Cyber Monday (BFCM) campaign</li>
-			<li>Designed sitemap, wireframe, prototype and UI of CheapOAir web page and mobile app for better user engagement.</li>
-			<li>Translated user stories and business requirements into effective designs.</li>
-			<li>Upskilled the team with the Figma software.</li>
-			</ul> 
-			</div>
+				<div className="reactive-container">
+					<h3 className={experienceIsVisible ? "reactive" : ""}>
+						Work Experience
+					</h3>
+					<h3 className={volunteerIsVisible ? "reactive" : ""}>
+						Volunteering and Leadership
+					</h3>
+					<h3 className={educationIsVisible ? "reactive" : ""}>Education</h3>
+					<h3>Resume</h3>
+				</div>
+				<div className="text-area">
+					<div ref={experience}>
+						<h2> UI/UX Designer Intern</h2>
+						<h4>Foreportal India Private Limited Gurgaon</h4>
+						<h5>20-06-2022 to 20-12-2022</h5>
 
-			
-			<h2> Java Intern</h2>
-			<h4>Allsoft Solutions and Services Private Limited, Chandigarh</h4>
-			<h5>15 June 2020 - 31 July 2020</h5>
-			<ul>
-			<li>
-				Developed project-Student Attendance Management System using Java and MySql Database</li></ul>
-			<div ref={volunteer}>
-			<h2>Exceutive member</h2>
-			<h4>Toastmasters Club , Chitkara University, Punjab</h4>
-			<h5> 2019 - Present</h5>
-			<ul>
-			<li>
-			Organized Colloquio Competitions and Table's Topic Session</li></ul>
-			<br></br>
-			<h2>Editor</h2>
-			<h4>Inner wheel club utkarsh, Ambala Cantt</h4>
-			<h5>2021 - Present</h5>
-			<ul>
-			<li>
-			Handled social media https://www.instagram.com/iwc_utkarsh/
-			</li>
-			<li>
-			Organised plantation drives.
-			</li>
-			<li>
-			Conducted awareness drive about begging and girl child education
-			</li>
-			<li>
-			Organised National level Talent Hunt and Poetry Competition.
-			</li>
-			</ul>
-			<h2>Volunteer</h2>
-			<h4>Swarmani Youth Welfare Association, Chandigarh</h4>
-			<h5>March 2020 - Present</h5>
-			<ul>
-			<li>
-			Organising member for the camp ‘My Policy, My Right’-to ensure the schemes and initiatives reach the beneficiaries.
-			</li>
-			</ul>
-			</div>
+						<ul>
+							<li>
+								Carried out competitive analysis, qualitative research,
+								brainstorming, and ideation for Black Friday Cyber Monday (BFCM)
+								campaign
+							</li>
+							<li>
+								Designed sitemap, wireframe, prototype and UI of CheapOAir web
+								page and mobile app for better user engagement.
+							</li>
+							<li>
+								Translated user stories and business requirements into effective
+								designs.
+							</li>
+							<li>Upskilled the team with the Figma software.</li>
+						</ul>
+					</div>
 
-			<div ref={education}>
-			<h2> B.E. CSE specilisation in UX/UI</h2>
-			<h4>Chitkara University Punjab</h4>
-			<h5> August 2019 - june 2023</h5>
-			<ul>
-			<li>
-			CGPA - 9.78 (till 6th Semester)
-			</li>
-			<li>1st Price in CSI Tech Enclave 2021 in Tech Bolt quiz
-			</li>
-			</ul>
-			</div>
-			<h2>D.C. Montessori Senior Secondary School</h2>
-			<h4> Chandigarh</h4>
-			<h5>2017 - 2019</h5>
-			<h2>Convent of jesus and Mary school</h2>
-			<h4>Ambala</h4>
-			<h5>Untill 2017</h5>
-			</div>
+					<h2> Java Intern</h2>
+					<h4>Allsoft Solutions and Services Private Limited, Chandigarh</h4>
+					<h5>15 June 2020 - 31 July 2020</h5>
+					<ul>
+						<li>
+							Developed project-Student Attendance Management System using Java
+							and MySql Database
+						</li>
+					</ul>
+					<div ref={volunteer}>
+						<h2>Exceutive member</h2>
+						<h4>Toastmasters Club , Chitkara University, Punjab</h4>
+						<h5> 2019 - Present</h5>
+						<ul>
+							<li>
+								Organized Colloquio Competitions and Table's Topic Session
+							</li>
+						</ul>
+						<br></br>
+						<h2>Editor</h2>
+						<h4>Inner wheel club utkarsh, Ambala Cantt</h4>
+						<h5>2021 - Present</h5>
+						<ul>
+							<li>
+								Handled social media https://www.instagram.com/iwc_utkarsh/
+							</li>
+							<li>Organised plantation drives.</li>
+							<li>
+								Conducted awareness drive about begging and girl child education
+							</li>
+							<li>
+								Organised National level Talent Hunt and Poetry Competition.
+							</li>
+						</ul>
+						<h2>Volunteer</h2>
+						<h4>Swarmani Youth Welfare Association, Chandigarh</h4>
+						<h5>March 2020 - Present</h5>
+						<ul>
+							<li>
+								Organising member for the camp ‘My Policy, My Right’-to ensure
+								the schemes and initiatives reach the beneficiaries.
+							</li>
+						</ul>
+					</div>
+
+					<div ref={education}>
+						<h2> B.E. CSE specilisation in UX/UI</h2>
+						<h4>Chitkara University Punjab</h4>
+						<h5> August 2019 - june 2023</h5>
+						<ul>
+							<li>CGPA - 9.78 (till 6th Semester)</li>
+							<li>1st Price in CSI Tech Enclave 2021 in Tech Bolt quiz</li>
+						</ul>
+					</div>
+					<h2>D.C. Montessori Senior Secondary School</h2>
+					<h4> Chandigarh</h4>
+					<h5>2017 - 2019</h5>
+					<h2>Convent of jesus and Mary school</h2>
+					<h4>Ambala</h4>
+					<h5>Untill 2017</h5>
+				</div>
 			</div>
 
 			<Footer />
